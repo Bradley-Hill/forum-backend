5635fb498e04ba34c8f03644d2ec56fa49312e42
Bradley-Hill <brad.michael.hill@gmail.com>
2026-02-16 09:10:03 +0100

    forum-backend: create the DB connection/pool

---
+++ src/db/pool.ts
@@ -0,0 +1,13 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.SUPABASE_CONNECTION_STRING,
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
+export default pool;
